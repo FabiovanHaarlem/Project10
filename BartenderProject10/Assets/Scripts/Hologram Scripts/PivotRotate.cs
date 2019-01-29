d289ab8e017b8f088fde896fce0095cfc5234c35
Rico de Bruin <rico-debruin@planet.nl>
2019-01-29 21:05:36 +0100

    Import + Level scene change
    Imported the holorgram shader and a Prefab for the planet solar system, put everything in different maps ( you can recognise them with the HOLOGRAM name ) and added all the chais, lamps, couches and the lamps in the Level Build scene

---
+++ BartenderProject10/Assets/Scripts/Hologram Scripts/PivotRotate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PivotRotate : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 10;
+
+    void Update()
+    {
+        transform.RotateAround(target.position, target.up, speed * Time.deltaTime);
+    }
+}
